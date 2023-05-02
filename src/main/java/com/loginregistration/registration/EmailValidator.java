0ab88101879b4aee0bd86f21eccd515291d30385
oscarpetter <oscarnordgren94@gmail.com>
2023-05-02 15:47:22 +0200

    set regex matcher to user email validation

--- src/main/java/com/loginregistration/registration/EmailValidator.java
+++ src/main/java/com/loginregistration/registration/EmailValidator.java
@@ -3,13 +3,16 @@ package com.loginregistration.registration;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidator implements Predicate<String> {
 
+    private static Pattern regex = Pattern.compile("[-@._a-zA-Z0-9]{4,}");
     @Override
     public boolean test(String s) {
         //TODO: validate with regex
-        return true;
+
+        return regex.matcher(s).matches();
     }
 }
